04367133170461688a58e8823296249225709371
br4ck <Qmanqtip12!!!>
2021-04-19 02:11:36 -0700

    php fix

--- home.php
+++ home.php
@@ -31,7 +31,7 @@ if (!isset($_SESSION['loggedin'])) {
 				if(file_exists($_SESSION['name']))
 				{
 					echo 'Made!';
-					echo 'Bot Dashboard Link: <button onclick="window.location.href=' . "'" . "/" . $_SESSION['name'] . "'" .'">Continue</button>';
+					echo '\n Bot Dashboard Link: <button onclick="window.location.href=' . "'" . "/" . $_SESSION['name'] . "'" .'">Continue</button>';
 				}
 				else
 				{
