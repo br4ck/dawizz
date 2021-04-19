44e3f357bd505de3abbb269b01f2dc05a2fa4631
br4ck <Qmanqtip12!!!>
2021-04-19 11:58:44 -0700

    edit to php

--- profile.php
+++ profile.php
@@ -29,7 +29,7 @@ $stmt->close();
 	<head>
 		<meta charset="utf-8">
 		<title>Profile Page</title>
-		<link href="style.css" rel="stylesheet" type="text/css">
+		<link href="main.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
 	<body class="loggedin">
